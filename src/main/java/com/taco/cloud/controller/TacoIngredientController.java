b7e99c6e9cf35e0ff96c81eac19c68e083bdc7b8
Nensi Skenderi <nensiskenderi20@gmail.com>
2022-11-05 13:23:52 +0100

    RestTemplate getForEntity example usage

--- src/main/java/com/taco/cloud/controller/TacoIngredientController.java
+++ src/main/java/com/taco/cloud/controller/TacoIngredientController.java
@@ -5,9 +5,14 @@ import com.taco.cloud.model.TacoIngredient;
 import com.taco.cloud.repo.JpaTacoIngredientRepository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/ingredients")
@@ -28,5 +33,11 @@ public class TacoIngredientController {
                 Sort.by("id").descending());
 
         return tacoIngredientRepository.findAll(pageRequest).getContent();
+    }
+
+    @GetMapping("/{id}")
+    private ResponseEntity<TacoIngredient> getIngredientById(@PathVariable("id") String id){
+        Optional<TacoIngredient> tacoIngredient = tacoIngredientRepository.findById(id);
+        return tacoIngredient.map(ResponseEntity::ok).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
 }
--- src/main/java/com/taco/cloud/controller/resttemplate/GetResources.java
+++ src/main/java/com/taco/cloud/controller/resttemplate/GetResources.java
@@ -1,7 +1,9 @@
 package com.taco.cloud.controller.resttemplate;
 
 import com.taco.cloud.model.Taco;
+import com.taco.cloud.model.TacoIngredient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,26 +14,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/get")
 public class GetResources {
 
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping("/{tacoId}")
-    private Taco setTacoId(@PathVariable("tacoId") Long id) {
-        return getTacoById(id);
+    @GetMapping("/taco/{tacoId}")
+    private Taco getTacoId(@PathVariable("tacoId") Long id) {
+        return getTacoByIdUsingRestTemplate(id);
     }
 
-    public Taco getTacoById(Long tacoId) {
-//        return restTemplate.getForObject("http://localhost:8080/design/taco/{tacoId}",
-//                TacoIngredient.class, ingredientId);
+    @GetMapping("/ingredient/{tacoIngredientId}")
+    private TacoIngredient getTacoIngredientById(@PathVariable("tacoIngredientId") Long id) {
+        return getTacoIngredientByIdUsingRestTemplate(id);
+    }
 
-        //we can also use a Map to specify the URL variables
+    public Taco getTacoByIdUsingRestTemplate(Long tacoId) {
         Map<String, String> variables = new HashMap<>();
         variables.put("tacoId", tacoId.toString());
 
         return restTemplate.getForObject("http://localhost:8080/design/taco/{tacoId}",
                 Taco.class, variables);
+    }
+
+    public TacoIngredient getTacoIngredientByIdUsingRestTemplate(@PathVariable("id") Long id) {
+        ResponseEntity<TacoIngredient> responseEntity =
+                restTemplate.getForEntity("http://localhost:8080/ingredients/{id}", TacoIngredient.class, id);
+        // getForEntity gives us access to additional response details
+
+        System.out.println(responseEntity.getHeaders());
+
+        return responseEntity.getBody();
     }
 }
